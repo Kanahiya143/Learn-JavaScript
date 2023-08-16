304fa1ce96549f675ecb38f23fcc2e7087e17b9f
Kanahiya143 <79072216+Kanahiya143@users.noreply.github.com>
2023-08-16 12:30:56 +0000

    container config done

---
+++ 0_basics/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict" // treat all js code as new ver
+
+// alert (3+3) // give o/p in browsr at that time we use nodejs
+
+console.log(3+3 ) // give o/p in nodejs
+
+let name = "kanhaiya"
+let age = 22
+let isLogged = false 
+
+
+// number => 2 to power 53
+//bigint 
+//string => ""
+//bolean => true/false 
+// null => standalone value
+// undefine => value not assign
+// symbol => unique
+
+
+// object 
+
+console.log(typeof "Kanahiya");  // string
+console.log(typeof age); // number 
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
