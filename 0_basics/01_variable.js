bcde3c3ff89b69ae5c8937dbd456f58ebe22283f
Kanahiya143 <79072216+Kanahiya143@users.noreply.github.com>
2023-08-16 11:18:07 +0000

    commit config done

---
+++ 0_basics/01_variable.js
@@ -0,0 +1,17 @@
+const myName = Kanahiya
+let myEmail = "kan@g.com"
+var myPassword = "1234"
+myCity = "Delhi"
+let myState ;
+
+
+//myName = Sahni // not allowed to reasign in const variable
+
+myEmail = "sah@g.com"
+myPassword = "5678"
+myCity = "gurugram"
+
+console.log(myName);
+
+console.table([myName, myEmail, myPassword, myCity, myState])
+
