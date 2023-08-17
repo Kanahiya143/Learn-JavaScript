ee58a0b4f8367a2e7be592f547b2fa311706f7c7
Kanahiya143 <79072216+Kanahiya143@users.noreply.github.com>
2023-08-17 11:38:28 +0000

    comparision done

--- 0_basics/03_conversionofdt.js
+++ 0_basics/03_conversionofdt.js
@@ -26,3 +26,54 @@ console.log(strInNumber)
 console.log(typeof strInNumber)
 
 // in string if there r num only o/p is num otherwise NaN but dtyp is num alwys
+
+//---------------------------Operation-------------------------------//
+
+let str1 = "hello "
+let str2 = "kanhaiya !"
+let str3 = str1 + str2 ;
+console.log(str3)
+
+
+
+console.log(1 + "2")
+console.log("1" + 2)
+console.log("1" + "2")
+console.log(1 + "2" + 3)
+console.log(1 + 2 + "3")
+console.log("1" + 2 + 3)
+
+
+//--------------------comparision--------------------------------//
+
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+
+console.log(undefined > 0)
+console.log(undefined == 0)
+console.log(undefined >= 0)
+
+// for comparision null treat as zero but for equality it works differently 
+
+let a = 20 
+a++
+console.log("a = " + a)
+
+let b = 20 
+++b
+console.log( "b = " + b)
+
+let p = 20
+let q = p++
+
+console.log("p = " + p  + ", q = " + q)
+
+let s = 20
+let t = ++s
+
+console.log("s = " + s  + ", t = " + t)
+
+
