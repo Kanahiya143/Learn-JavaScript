e500d49a2dd6167f82529aa7f64b0bea905151f0
Kanahiya143 <79072216+Kanahiya143@users.noreply.github.com>
2023-08-17 08:29:30 +0000

    conversion config done

---
+++ 0_basics/03_conversionofdt.js
@@ -0,0 +1,28 @@
+
+// number to string 
+
+let age = 33
+let ageInString = String(age)
+console.log(ageInString);
+console.log(typeof ageInString);
+
+/**? result same but datatype is string */
+
+// number and string to boolean
+
+let number = 2
+let numberInBoolean = Boolean(number)
+console.log(numberInBoolean)
+console.log(typeof numberInBoolean)
+
+// string/number => true 
+// 0 null undefined empty string => false
+
+// String to Number 
+
+let str = "abc"
+let strInNumber = Number(str)
+console.log(strInNumber)
+console.log(typeof strInNumber)
+
+// in string if there r num only o/p is num otherwise NaN but dtyp is num alwys
