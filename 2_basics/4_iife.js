927af0f6d2c8d8ec11cde44ee6a089ac16cad8e4
Kanahiya143 <79072216+Kanahiya143@users.noreply.github.com>
2023-08-26 11:17:17 +0000

    iffe done

--- 2_basics/4_iife.js
+++ 2_basics/4_iife.js
@@ -3,7 +3,7 @@
 (function dbConnection (){
     // named IIFE
     console.log(`DB CONNECTED`);
-})(); // ; used for break the function becoz other iife will not executed 
+})(); // ; used for break the function becoz other iife will not executed so use ;
 
 ( (name) => {
     console.log(`DB CONNECTED, ${name}`);
