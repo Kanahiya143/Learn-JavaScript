5e8d6356631531f57679c541d68da39def6ec41a
Kanahiya143 <79072216+Kanahiya143@users.noreply.github.com>
2023-09-06 12:01:54 +0000

    filter done

---
+++ 4_iterations/5_filter.js
@@ -0,0 +1,54 @@
+// const webTech =  ["HTML", "CSS", "JavaSript", "PHP", "MySQL"]
+
+// const values = webTech.forEach((item) => { // foreach loop is not return the value
+//     return item 
+// })
+
+// console.log(values);
+
+
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const myNum = num.filter( (num) => num > 4)
+
+// console.log(myNum);
+
+const newNum = num.filter( (num) => { // if we create the scope need to return the value for print
+    return num > 4
+})
+
+// console.log(newNum);
+
+// below is expriment but not success
+
+// let byForEach = []
+
+// byForEach = num.forEach( (num) => { // if we create the scope need to return the value for print
+//     if (num > 4){
+//        return byForEach.push(num)
+//     }
+// })
+
+// console.log(byForEach);
+
+
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+ const newBook = books.filter( (bk) => bk.publish > 1990)
+
+//  console.log(newBook);
+
+ const history = books.filter( (bk) => bk.genre === 'History')
+
+ console.log(history);
